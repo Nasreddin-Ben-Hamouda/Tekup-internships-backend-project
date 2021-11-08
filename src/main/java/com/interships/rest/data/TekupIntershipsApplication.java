0cc074f04137d21f163d3b664bcc71240a19f9cd
Nasr Eddine <nasreddinbenhamouda@gmail.com>
2021-11-09 00:52:20 +0100

    Project initialization

---
+++ src/main/java/com/interships/rest/data/TekupIntershipsApplication.java
@@ -0,0 +1,13 @@
+package com.interships.rest.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekupIntershipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekupIntershipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interships/rest/data/TekupIntershipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interships.rest.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekupIntershipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
